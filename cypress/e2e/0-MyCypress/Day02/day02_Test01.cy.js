5dc6923bd4cc80c711816a1160b768c11180e052
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-05 17:50:25 +0300

    first commit

---
+++ cypress/e2e/0-MyCypress/Day02/day02_Test01.cy.js
@@ -0,0 +1,9 @@
+context('context kullanimi', () => {
+    it('google ziyaret', () => {
+        cy.visit('https://www.google.com')
+    });
+    it('title', () => {
+        cy.visit('https://www.google.com')
+        cy.title().should('include','Google')
+    });
+});
