d5a715fcbb6152bdf5e27eb7caaf1f7661fdda62
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-05 17:49:55 +0300

    first commit

---
+++ cypress/e2e/0-MyCypress/Day02/day02_LoginTesti.cy.js
@@ -0,0 +1,23 @@
+describe('Login testi', () => {
+    it('qualitydemy login testi', () => {
+        cy.visit('https://qa.qualitydemy.com/')
+        cy.get('.btn-sign-in-simple').click()
+
+        // 2.yontem
+        // cy.contains('Login').click()
+
+        // 3.yontem
+        // cy.contains('log in', {matchCase:false}).click() buyuk-kucuk harf bakmaksızın elementi bulur ve tiklar
+
+        cy.get('#login-email').type('mb@babayigit.net')
+        cy.get('#login-password').type('M123456.b')
+        cy.get('#sign_up > :nth-child(3) > .btn').click()
+
+        //2. yontem
+       // cy.get('#login-email').type('mb@babayigit.net')
+       // cy.get('#login-password').type('M123456.b{enter}')
+    });
+});
+
+
+
