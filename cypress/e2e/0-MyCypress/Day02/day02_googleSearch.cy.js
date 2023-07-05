e34bd5ef70c13b8c5aa48dd78d4ac23a5fc2b348
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-05 17:49:17 +0300

    first commit

---
+++ cypress/e2e/0-MyCypress/Day02/day02_googleSearch.cy.js
@@ -0,0 +1,12 @@
+describe('', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    });
+    it('google da 3 Idiots', () => {
+        cy.get('#APjFqb').type('3 idiots{enter}')
+    });
+
+    it('tenet ara', () => {
+        cy.get('#APjFqb').type('Tenet{enter}')
+    });
+});
