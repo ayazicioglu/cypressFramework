0598144bbc5ad28ca4af771c912105ac1104ee41
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-15 15:30:31 +0300

    15.7.23

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:"https://www.google.com"
+    baseUrl:"https://www.google.com",
+    projectId: "zoc9iw",
+    "video":false, //video kaydının alınmasını engeller false yerine true yapılarak kayıt alınması saglanabillir
+    "retire":2 // fail olan testlerin 2 kere daha çalışmasını sağlar
   },
 });
 
