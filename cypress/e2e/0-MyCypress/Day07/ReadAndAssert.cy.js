3281067d33250046bfdbaaae27f639b1c46a3a6d
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-16 15:33:27 +0300

    16.7.23

---
+++ cypress/e2e/0-MyCypress/Day07/ReadAndAssert.cy.js
@@ -0,0 +1,12 @@
+import { use } from "chai";
+
+describe('Read and Assert from Files', () => {
+    it('Read and Assert', () => {
+        cy.readFile('cypress/fixtures/users.json').then(
+            (userdata)=>{
+            expect(userdata[7].name).to.equal('Nicholas Runolfsdottir V')
+            expect(userdata[7].username).to.equal('Maxime_Nienow')
+            expect(userdata[7].email).to.equal('Sherwood@rosamond.me')
+        })
+    });
+});
