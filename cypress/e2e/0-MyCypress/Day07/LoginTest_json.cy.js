6fdf2a317f593746d0e4863ad895059f6629feee
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-16 15:29:43 +0300

    16.7.23

---
+++ cypress/e2e/0-MyCypress/Day07/LoginTest_json.cy.js
@@ -0,0 +1,15 @@
+describe('Json ile login testi', () => {
+    before(function() {
+        cy.fixture('qualitydemy').then(function(data){
+            this.data=data
+            
+        })
+
+    });
+
+
+    it('Login testi', () => {
+        cy.visit(this.data.url)
+        cy.contains('log in',{matchCase:false}).click()
+    });
+});
