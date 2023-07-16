bc2c1039502dd15b0aec9f3eccb1b5d60010acd3
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-16 15:31:20 +0300

    16.7.23

---
+++ cypress/e2e/POM/Login_POM.cy.js
@@ -0,0 +1,20 @@
+import Homepage from "../POM/Homepage.cy";
+import LoginPage from "./Loginpage.cy";
+
+describe('POM ile login', () => {
+    it('Login testi', () => {
+        const homepage= new Homepage()
+        const loginpage= new LoginPage()
+    
+
+       homepage.getVisit() // cy.visit('https://qa.qualitydemy.com/')
+       homepage.getLoginlink().click() // cy.get('.btn-sign-in-simple').click()
+
+       loginpage.getUserEmail()
+       loginpage.getPasswordBox()
+       loginpage.getLoginButton()
+       loginpage.getAssert()
+    });
+
+    
+});
