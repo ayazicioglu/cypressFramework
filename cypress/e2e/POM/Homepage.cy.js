d183b2a5dddee091fca0769dca5d0a876046abd4
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-07-16 15:30:57 +0300

    16.7.23

---
+++ cypress/e2e/POM/Homepage.cy.js
@@ -0,0 +1,11 @@
+class Homepage{
+    getLoginlink(){
+        return   cy.get('.btn-sign-in-simple')
+    }
+
+    getVisit(){
+        cy.visit('https://qa.qualitydemy.com/')
+    }
+
+}
+export default Homepage
