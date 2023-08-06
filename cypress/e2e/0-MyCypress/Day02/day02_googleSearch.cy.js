4822574d968478c553acf22465562720b4a36354
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-08-06 23:35:22 +0300

    5.8.23 ayazicioglu

--- cypress/e2e/0-MyCypress/Day02/day02_googleSearch.cy.js
+++ cypress/e2e/0-MyCypress/Day02/day02_googleSearch.cy.js
@@ -5,6 +5,7 @@ describe('', () => {
     it('google da 3 Idiots', () => {
         cy.get('#APjFqb').type('3 idiots{enter}')
     });
+    
 
     it('tenet ara', () => {
         cy.get('#APjFqb').type('Tenet{enter}')
