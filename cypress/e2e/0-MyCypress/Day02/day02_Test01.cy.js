9a13f6fcaf3922ae0523bb5969840bfa738c4449
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-08-06 23:36:11 +0300

    5.8.23 ayazicioglu

--- cypress/e2e/0-MyCypress/Day02/day02_Test01.cy.js
+++ cypress/e2e/0-MyCypress/Day02/day02_Test01.cy.js
@@ -2,6 +2,7 @@ context('context kullanimi', () => {
     it('google ziyaret', () => {
         cy.visit('https://www.google.com')
     });
+    
     it('title', () => {
         cy.visit('https://www.google.com')
         cy.title().should('include','Google')
