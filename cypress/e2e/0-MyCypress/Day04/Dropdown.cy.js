365a3368862186f13bfccd7fad9fea1f7e6f3d77
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-08-17 23:49:06 +0300

    17.8.23 ayazicioglu: toshiba

--- cypress/e2e/0-MyCypress/Day04/Dropdown.cy.js
+++ cypress/e2e/0-MyCypress/Day04/Dropdown.cy.js
@@ -5,5 +5,7 @@ describe('Dropdown Locate', () => {
         cy.get('#dropdown').select('Option 1').should('have.value',('1'))
         cy.get('#dropdown').select('Option 2').should('have.value',('2'))
         
+
+        
     });
 });
